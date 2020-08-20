547d7266cab55afd950ec113d78e4944df1dd50b
lewy95 <171133755@qq.com>
2020-08-20 18:11:33 +0800

    add snowflake id

---
+++ src/main/java/cn/xzxy/lewy/dscross/DatasourceCrossApplication.java
@@ -0,0 +1,13 @@
+package cn.xzxy.lewy.dscross;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasourceCrossApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatasourceCrossApplication.class, args);
+    }
+
+}
